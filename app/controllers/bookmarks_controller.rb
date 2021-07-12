cdfa97cb812d23fe65272d39dd3333c7e0b6702d
Namkhing Lee <nonlapat.lee@gmail.com>
2021-07-12 12:08:12 +0700

    Rake fixed

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,37 @@
+class BookmarksController < ApplicationController
+  before_action :find_bookmark, only: :destroy
+  before_action :find_list, only: [:new, :create]
+  def new
+    @bookmark = Bookmark.new
+  end
+
+  def create
+    @bookmark = Bookmark.new(bookmark_params)
+    # @movie = Movie.find(params[:movie_id])
+    @bookmark.list = @list
+    # @bookmark.movie = @movie
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @bookmark.destroy
+    redirect_to list_path(@bookmark.list)
+  end
+
+  private
+
+  def find_list
+    @list = List.find(params[:list_id])
+  end
+
+  def find_bookmark
+    @bookmark = Bookmark.find(params[:id])
+  end
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+Movie.destroy_all
+# List.destroy_all
+puts "🧹 Cleaning movies..."
+# the Le Wagon copy of the API
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+response = JSON.parse(URI.open(url).read)
+
+puts "🌱 Seeding...."
+response['results'].each do |movie_hash|
+  # create an instance with the hash
+  Movie.create!(
+    title: movie_hash['title'],
+    overview: movie_hash['overview'],
+    poster_url: "https://image.tmdb.org/t/p/w500" + movie_hash['poster_path'],
+    rating: movie_hash['vote_average']
+  )
+  puts "🍿 Add #{movie_hash['title']}.... "
+end
+puts "-------------------------"
+puts "✅ Finish seeding!"
